20b66f99226493369d1b6450e90f81e5113443b5
Holy Lovenia <holy.lovenia@gmail.com>
2017-03-26 21:02:42 +0700

    Added AnimalBehavior

---
+++ src/virtualzoo/animal/behavior/AnimalBehavior.java
@@ -0,0 +1,29 @@
+package virtualzoo.animal.behavior;
+
+/**
+ * Kelas AnimalBehavior menentukan perilaku hewan.
+ * yang ada di dalam Zoo.
+ *
+ * @author Holy Lovenia - 13515113
+ * @version 3.0
+ * @since   3.0
+ */
+public abstract class AnimalBehavior {
+    /**
+     * Atribut menentukan apakah perilaku Animal jinak atau liar.
+     */
+    protected boolean is_wild;
+
+    /**
+     * Mengembalikan perilaku Animal
+     * @return Nilai is_wild
+     */
+    public boolean GetBehavior() {
+        return is_wild;
+    }
+
+    /**
+     * Menetapkan perilaku Animal
+     */
+    public abstract void SetBehavior();
+}
