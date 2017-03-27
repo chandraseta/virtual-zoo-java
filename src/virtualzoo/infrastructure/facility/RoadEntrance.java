ecd3aa95ed4783ea4af5cd50744c00fba06ff52b
Oktavianus Handika <Oktavianus Handika>
2017-03-27 15:58:13 +0700

    Added Restaurant and Road

---
+++ src/virtualzoo/infrastructure/facility/RoadEntrance.java
@@ -0,0 +1,23 @@
+package virtualzoo.infrastructure.facility;
+
+import virtualzoo.infrastructure.facility.Road;
+
+/**
+ * Kelas RoadEntrance mendefinisikan sebuah jalan masuk di dalam Zoo.
+ *
+ * @author Oktavianus Handika - 13515035
+ * @version 3.0
+ * @since   3.0
+ */
+public class RoadEntrance extends Road {
+    /**
+     * Constructor
+     *
+     * Kelas RoadEntrance mendefinisikan atribut untuk jalan masuk.
+     */
+    RoadEntrance (boolean accessible, String _name) {
+        super(accessible, _name);
+        is_entrance = true;
+    }
+
+}
